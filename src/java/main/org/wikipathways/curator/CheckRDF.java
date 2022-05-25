ca07d8c078a1bf0bc14537ccd02e0525ca8b5473
Egon Willighagen <egon.willighagen@gmail.com>
2022-05-25 20:37:36 +0200

    Match the WP website color scheme

--- src/java/main/org/wikipathways/curator/CheckRDF.java
+++ src/java/main/org/wikipathways/curator/CheckRDF.java
@@ -206,7 +206,7 @@ public class CheckRDF {
         } else {
           reportStatus.println("status=✓");
           reportJSON.println("  \"message\": \"success\",");
-          reportJSON.println("  \"color\": \"green\"");
+          reportJSON.println("  \"color\": \"brightgreen\"");
         }
         reportJSON.println("}");
 
